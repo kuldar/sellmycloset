5359296b0aa7072d8a95c9183a4cc937f721aac0
Kuldar Kalvik <hi@kuldar.com>
2016-09-04 20:17:25 +0300

    Remove helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def render_icon(icon)
-    render file: Rails.root.join('app/assets/images', icon) if Rails.application.assets.find_asset icon
-  end
+	# def render_icon(icon)
+ #    render file: Rails.root.join('app/assets/images', icon) if Rails.application.assets.find_asset icon
+ #  end
 end
