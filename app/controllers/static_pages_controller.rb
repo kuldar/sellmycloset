6db8def5b02c6ad72503144a50e198ec7a64ddff
Kuldar Kalvik <hi@kuldar.com>
2016-09-05 14:43:51 +0300

    Minor tweaks

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
 	def home
-		if user_signed_in?
+		if user_signed_in? && current_user.feed.any?
 			@products = current_user.feed
 		else
 			@products = Product.all
