6ce9ad353c79afcc68bf6f68079363a253ec719f
Kuldar Kalvik <hi@kuldar.com>
2016-09-09 00:50:46 +0300

    Add search bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
 	# def render_icon(icon)
  #    render file: Rails.root.join('app/assets/images', icon) if Rails.application.assets.find_asset icon
  #  end
+
+	def options_for_tags
+		ActsAsTaggableOn::Tag.all
+	end
+	
 end
