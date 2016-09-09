4ab7b02dea090b5fb6b2faf00873f54d101e0ed9
Kuldar Kalvik <hi@kuldar.com>
2016-09-09 10:49:38 +0300

    Start localizing the app

--- app/controllers/product_images_controller.rb
+++ app/controllers/product_images_controller.rb
@@ -3,14 +3,7 @@ class ProductImagesController < ApplicationController
 	def create
 		params[:product_images].each do |image|
 			@product_image = ProductImage.create(image: image)
-			if @product_image.save
-				respond_to do |format|
-					format.html { render json: @product_image.to_jq_upload, content_type: 'text/html', layout: false }
-					format.json { render json: @product_image.to_jq_upload }
-				end
-			else
-				render json: { error: @product_image.errors.full_messages }, status: 304
-			end
+			@product_image.save
 		end
 	end
 
