158fded55fd6cc5f47661def84759560ef63ea65
Kuldar Kalvik <hi@kuldar.com>
2016-09-12 20:35:55 +0300

    Add correct references to Swipebox assets

--- app/assets/javascripts/lib/jquery.swipebox.js
+++ app/assets/javascripts/lib/jquery.swipebox.js
@@ -8,7 +8,6 @@
 		var ui,
 			defaults = {
 				useCSS : true,
-				useSVG : true,
 				initialIndexOnArray : 0,
 				removeBarsOnMobile : true,
 				hideCloseButtonOnMobile : false,
@@ -33,7 +32,6 @@
 			selector = elem.selector,
 			isMobile = navigator.userAgent.match( /(iPad)|(iPhone)|(iPod)|(Android)|(PlayBook)|(BB10)|(BlackBerry)|(Opera Mini)|(IEMobile)|(webOS)|(MeeGo)/i ),
 			isTouch = isMobile !== null || document.createTouch !== undefined || ( 'ontouchstart' in window ) || ( 'onmsgesturechange' in window ) || navigator.msMaxTouchPoints,
-			supportSVG = !! document.createElementNS && !! document.createElementNS( 'http://www.w3.org/2000/svg', 'svg').createSVGRect,
 			winWidth = window.innerWidth ? window.innerWidth : $( window ).width(),
 			winHeight = window.innerHeight ? window.innerHeight : $( window ).height(),
 			currentX = 0,
@@ -172,14 +170,6 @@
 				var $this = this, bg;
 
 				$( 'body' ).append( html );
-
-				if ( supportSVG && plugin.settings.useSVG === true ) {
-					bg = $( '#swipebox-close' ).css( 'background-image' );
-					bg = bg.replace( 'png', 'svg' );
-					$( '#swipebox-prev-arrow, #swipebox-next-arrow, #swipebox-close' ).css( {
-						'background-image' : bg
-					} );
-				}
 
 				if ( isMobile && plugin.settings.removeBarsOnMobile ) {
 					$( '#swipebox-bottom-bar, #swipebox-top-bar' ).remove();
